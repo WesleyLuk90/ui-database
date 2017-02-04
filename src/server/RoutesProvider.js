fd97503fdda0e13d1896567ea0ff43760314b014
Wesley Luk <wesleyluk@gmail.com>
2017-02-03 22:18:55 -0800

    Added database dependency

--- src/server/RoutesProvider.js
+++ src/server/RoutesProvider.js
@@ -2,8 +2,15 @@ const assert = require('assert');
 const Server = require('./Server');
 const express = require('express');
 const path = require('path');
+const Database = require('./Database');
 
 module.exports = class RoutesProvider {
+    constructor(database) {
+        assert.ok(database instanceof Database);
+
+        this.database = database;
+    }
+
     provide(server) {
         assert.ok(server instanceof Server);
 
