84a6a992cf6eac3f7eb787e0003cf73335c83ad2
Wesley Luk <wesleyluk@gmail.com>
2017-04-08 11:06:57 -0700

    Faster dev config for karma

---
+++ karma.dev.conf.js
@@ -0,0 +1,44 @@
+const webpack = require('webpack');
+const webpackConfig = require('./webpack.config');
+
+webpackConfig.entry = './spec/client/index.js';
+webpackConfig.plugins = [];
+webpackConfig.externals = {
+    cheerio: 'window',
+    'react/addons': true,
+    'react/lib/ExecutionEnvironment': true,
+    'react/lib/ReactContext': true,
+};
+webpackConfig.devtool = 'cheap-module-source-map';
+webpackConfig.output = {
+    path: `${__dirname}/build`,
+    filename: 'test-bundle.js',
+};
+const compiler = webpack(webpackConfig);
+
+compiler.watch({
+
+}, (err, stats) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    console.log(stats.toString({
+        chunks: false, // Makes the build much quieter
+        colors: true, // Shows colors in the console
+    }));
+});
+
+module.exports = function configure(config) {
+    config.set({
+        frameworks: ['jasmine'],
+        files: [
+            { pattern: './build/test-bundle.js', watched: true },
+        ],
+
+        preprocessors: {
+            './build/test-bundle.js': ['sourcemap'],
+        },
+        browsers: ['Chrome'],
+    });
+};
